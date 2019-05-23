e0524802657532f2bd6fa72bba2542756db854e4
Pierre Peigne <ppeigne@student.42.fr>
2019-05-23 16:22:50 +0200

    ContainerExecutor: -add binPath to nethods; -remove println/notes/betterfile

--- src/main/scala/container/ContainerExecutor.scala
+++ src/main/scala/container/ContainerExecutor.scala
@@ -2,51 +2,24 @@ package container
 
 import container.ImageBuilder.checkImageFile
 import scala.sys.process._
-import container.Status.note
-import better.files.{File => BFile, _}
+import java.io.File
 
 
 object ContainerExecutor {
-
-//  def executeContainerWithDocker(image: SavedDockerImage): Unit = {
-
-    def executeContainerWithDocker(image: BuiltDockerImage): Unit = {
-    note(" - execute container with Docker")
-    note(" -- checkImage")
-    checkImageFile(image.file)
-    val file = image.file.toScala.pathAsString
-
-      println(s"executeDocker : file: $file")
-
-    ("docker load -i" +  file).!!
-
-
-    note("cmd = " + image.command.mkString("'", " ", "'"))
-
-    if (image.command.isEmpty)
-      ("docker run " + image.imageName).!!
-    else {
-      val x = ("docker run " + image.imageName + " sh -c "+ image.command.mkString(""," ", "")) .!
-      println(x)
-    }
-//      ("docker run " + image.imageName + " sh -c "+ image.command.mkString("'"," ", "'")) .!
-
+    def executeContainerWithDocker(image: BuiltDockerImage)(binPath: File): Unit = {
+      checkImageFile(image.file)
+      val file = image.file.getAbsolutePath
+      ("docker load -i" +  file).!!
+      if (image.command.isEmpty)
+        ("docker run " + image.imageName).!!
+      else (binPath.getAbsolutePath +"docker run " + image.imageName + " sh -c "+ image.command.mkString(""," ", "")) .!
     }
 
-  def executeContainerWithoutDocker(image: BuiltPRootImage): Unit = {
-    note(" - execute container without Docker")
-    note(" -- checkImage")
+  def executeContainerWithoutDocker(image: BuiltPRootImage)(binPath: File): Unit = {
     checkImageFile(image.file)
     val path = image.file.getAbsolutePath + "/"
-    note(" -- prepareEnvVar")
-
     prepareEnvVariables(image.configurationData.Env)
-    note(" -- run proot")
-
-    note("cmd = " + image.command)
-    println("pRoot cmd args:\n" + "proot -r " + path + "rootfs/")
-    val status = Seq("proot", "-r", path + "rootfs/").union(image.command) .!!
-    println("status = " + status)
+    val status = Seq(binPath.getAbsolutePath + "proot", "-r", path + "rootfs/").union(image.command) .!!
   }
 
   def prepareEnvVariables(maybeArgs: Option[List[String]]) {
